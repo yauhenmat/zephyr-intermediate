dc57d0477fefb4e92e0446b242a88e933476dca5
Yauhen Matskevich <evgeniy.m@iomico.com>
2026-08-17 18:04:08 +0200

    Implement l1-task1

--- app/src/main.c
+++ app/src/main.c
@@ -5,30 +5,59 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
-#define PRIO_B 5
+#define PRIO_T_LOW 7
+#define PRIO_T_MED 5
+#define PRIO_T_HIGH 3
+#define PRIO_T_COOP (-1)
 
-void thread_a_fn(void *p1, void *p2, void *p3)
+void thread_t_low_fn(void *p1, void *p2, void *p3)
 {
     while (1) {
-        k_msleep(200);
-    }
-}
-
-void thread_b_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
+        LOG_INF("T_LOW running");
         k_msleep(300);
     }
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
-                NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
-                NULL, NULL, NULL, PRIO_B, 0, 0);
+void thread_t_med_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_MED running");
+        k_msleep(200);
+    }
+}
+
+void thread_t_high_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_HIGH running");
+        k_msleep(100);
+    }
+}
+
+void thread_t_coop_fn(void *p1, void *p2, void *p3)
+{
+    LOG_INF("COOP starting");
+    for(int i = 0; i < 5; i++) {
+        k_busy_wait(40000);
+        LOG_INF("COOP step %d - still holding CPU", i + 1);
+    }
+    LOG_INF("COOP yielding now");
+    k_yield();
+    LOG_INF("COOP done");
+}
+
+K_THREAD_DEFINE(thread_low, STACK_SIZE, thread_t_low_fn,
+                NULL, NULL, NULL, PRIO_T_LOW, 0, 0);
+K_THREAD_DEFINE(thread_med, STACK_SIZE, thread_t_med_fn,
+                NULL, NULL, NULL, PRIO_T_MED, 0, 0);
+K_THREAD_DEFINE(thread_high, STACK_SIZE, thread_t_high_fn,
+                NULL, NULL, NULL, PRIO_T_HIGH, 0, 0);
+K_THREAD_DEFINE(thread_coop, STACK_SIZE, thread_t_coop_fn,
+                NULL, NULL, NULL, PRIO_T_COOP, 0, 0);
 
 int main(void)
 {
+    LOG_INF("=== l1-task1: Thread Interleaving ===");
     return 0;
 }
 
